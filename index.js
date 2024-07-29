b640c4e4c287868d448ca13d597841865aca2126
leonidas <155920053+leonidas198@users.noreply.github.com>
2024-07-29 17:19:19 -0300

    react app ready

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 app.use('*', ( req, res ) => {
-    res.sendFile( path.join( __dirname, 'public/index.html' ) )
+    res.sendFile( path.join( __dirname, 'public/index.html' ) );
 });
 
 
